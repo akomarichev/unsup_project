624d36c3e5e144917c7062171ca51ebabc103868
Artem Komarichev <art@Artems-MacBook-Pro.local>
2016-08-09 18:09:22 -0400

    new modules which help in splitting and joining grid

---
+++ JoinGrid.lua
@@ -0,0 +1,52 @@
+require 'nn'
+
+local JoinGrid, Parent = torch.class('nn.JoinGrid', 'nn.Module')
+
+function JoinGrid:__init(patch_size, numberOfPatches)
+   Parent.__init(self)
+   self.size = torch.LongStorage()
+   self.patch_size = patch_size
+   self.numberOfPatches = numberOfPatches
+end
+
+function JoinGrid:updateOutput(input)
+
+   -- define the size of the joined table
+   self.output:resize(input[1]:size(1), self.patch_size * self.numberOfPatches, self.patch_size * self.numberOfPatches)
+
+   -- join grid
+   local patchNumber = 1
+   for x = 1, self.numberOfPatches do
+     for y = 1, self.numberOfPatches do
+       local currentInput = input[patchNumber]
+       self.output[{ {}, {(x-1) * self.patch_size + 1, x * self.patch_size}, {(y-1) * self.patch_size + 1, y * self.patch_size} }]:copy(currentInput)
+       patchNumber = patchNumber + 1
+     end
+   end
+
+   return self.output
+end
+
+
+function JoinGrid:updateGradInput(input, gradOutput)
+
+   for i=1,#input do
+      if self.gradInput[i] == nil then
+         self.gradInput[i] = input[i].new()
+      end
+      self.gradInput[i]:resizeAs(input[i])
+   end
+
+   local currentOutput= {}
+   local patchNumber = 1
+   for x = 1, self.numberOfPatches do
+     for y = 1, self.numberOfPatches do
+       local currentOutput = input[patchNumber]
+       local currentGradInput = gradOutput[{ {}, {(x-1) * self.patch_size + 1, x * self.patch_size}, {(y-1) * self.patch_size + 1, y * self.patch_size} }]
+       self.gradInput[i]:copy(currentGradInput)
+       patchNumber = patchNumber + 1
+     end
+   end
+
+   return self.gradInput
+end
---
+++ SplitGrid.lua
@@ -0,0 +1,36 @@
+require 'nn'
+
+local SplitGrid, Parent = torch.class('nn.SplitGrid', 'nn.Module')
+
+function SplitGrid:__init(patch_size, numberOfPatches)
+   Parent.__init(self)
+   self.patch_size = patch_size
+   self.numberOfPatches = numberOfPatches
+end
+
+function SplitGrid:updateOutput(input)
+   local currentOutput= {}
+   for x = 1, self.numberOfPatches do
+     for y = 1, self.numberOfPatches do
+       currentOutput[#currentOutput+1] = input[{ {}, {(x-1) * self.patch_size + 1, x * self.patch_size}, {(y-1) * self.patch_size + 1, y * self.patch_size} }]
+     end
+   end
+   self.output = currentOutput
+   return self.output
+end
+
+function SplitGrid:updateGradInput(input, gradOutput)
+   if self.gradInput then
+      self.gradInput:resizeAs(input)
+
+      local patchNumber = 1
+      for x = 1, self.numberOfPatches do
+        for y = 1, self.numberOfPatches do
+          local currentGradInput = gradOutput[patchNumber]
+          self.gradInput[{ {}, {(x-1) * self.patch_size + 1, x * self.patch_size}, {(y-1) * self.patch_size + 1, y * self.patch_size} }]:copy(currentGradInput)
+          patchNumber = patchNumber + 1
+        end
+      end
+   end
+   return self.gradInput
+end
